53260346b871d210f8969e59ddc66bdde951e6ff
Huzaifa-code <huzaifaqureshi037@gmail.com>
2024-06-11 22:51:07 +0530

    FEAT: Added Sidebar Menu

--- App.js
+++ App.js
@@ -10,18 +10,29 @@ import ChatScreen from './screens/ChatScreen'
 import { UserProvider } from './context/UserContext';
 import VideoScreen from './screens/VideoScreen';
 import { PermissionsAndroid, Platform } from 'react-native';
-
-
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Alert, Button } from 'react-native';
 import { useEffect } from 'react';
+import DrawerMenu from './components/DrawerMenu/DrawerMenu';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const globalScreenOptions = {
   headerStyle: { backgroundColor: "#381fd1" },
   headerTitleStyle: {color: "white"},
   headerTintColor: "white",
 };
+
+const DrawerNavigator = () => (
+  <Drawer.Navigator drawerContent={props => <DrawerMenu {...props} />}>
+    <Drawer.Screen name='Home' component={HomeScreen} />
+    <Drawer.Screen name='AddChat' component={AddChatScreen} />
+    <Drawer.Screen name='Chat' component={ChatScreen} />
+    <Drawer.Screen name='Video' component={VideoScreen} />
+  </Drawer.Navigator>
+);
+
 
 export default function App() {
 
@@ -47,10 +58,11 @@ export default function App() {
         >
           <Stack.Screen name='Login' component={LoginScreen} />
           <Stack.Screen name='Register' component={RegisterScreen} />
-          <Stack.Screen name='Home' component={HomeScreen} />
+          {/* <Stack.Screen name='Home' component={HomeScreen} />
           <Stack.Screen name='AddChat' component={AddChatScreen} />
           <Stack.Screen name='Chat' component={ChatScreen} />
-          <Stack.Screen name='Video' component={VideoScreen} />
+          <Stack.Screen name='Video' component={VideoScreen} /> */}
+          <Stack.Screen name='Main' component={DrawerNavigator} options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
     </UserProvider>
